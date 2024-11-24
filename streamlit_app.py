dbd30e4afbd686b63b53ddf26172fd0e8c72bd5d
Yuli Saavedra <84823922+tatiana292002@users.noreply.github.com>
2024-11-24 18:34:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,8 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','""" + name_on_order + """') """
 
-    # Uncomment this line to write ingredients
-    # st.write(ingredients_string)
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
