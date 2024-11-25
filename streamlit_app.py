ecd549ba124ea6bcffe93bb50804d8d286934d24
Yuli Saavedra <84823922+tatiana292002@users.noreply.github.com>
2024-11-24 19:28:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ if ingredients_list:
         # Concatenate selected ingredients to the string
         ingredients_string += fruit_chosen + ', '
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for', fruit_chosen,'is',search on,'.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(f"{fruit_chosen} Nutrition Information")
         try:
