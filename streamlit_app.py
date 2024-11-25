a1f708fe358c011ad5ffa28ce25099fb48c82594
Yuli Saavedra <84823922+tatiana292002@users.noreply.github.com>
2024-11-24 19:27:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,27 +24,26 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 
 # Convert Snowpark DataFrame to Pandas DataFrame
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
+#st.stop
 
-# Multiselect for ingredients
+
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    pd_df['FRUIT_NAME'].tolist(),  # Use the list of fruit names from Pandas DataFrame
-    max_selections=5
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
 )
 
-# If ingredients are selected
 if ingredients_list:
-    ingredients_string = ''  # Initialize the ingredients string
-
+    ingredients_string = '' 
+    
     for fruit_chosen in ingredients_list:
         # Concatenate selected ingredients to the string
         ingredients_string += fruit_chosen + ', '
 
-        # Get the SEARCH_ON value for the chosen fruit
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for', fruit_chosen,'is',search on,'.')
 
-        # Display the selected fruit's nutrition information
         st.subheader(f"{fruit_chosen} Nutrition Information")
         try:
             # Make API request using the SEARCH_ON value
