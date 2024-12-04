204a55af6070fab354c168eeb4407b5910b218fe
Yuli Saavedra <84823922+tatiana292002@users.noreply.github.com>
2024-12-04 10:03:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 st.title(":cup_with_straw: Pending smoothie orders :cup_with_straw:")
 st.write("""Orders that need to be filled.""")
 
 
-
-session= get_active_session()
+cnx = st.connection("snowflake")
+session= cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 editable_df = st.data_editor(my_dataframe)
 
