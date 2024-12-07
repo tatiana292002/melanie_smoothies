ea629ee08e41f4b455c8edae416550b0f578438e
Yuli Saavedra <84823922+tatiana292002@users.noreply.github.com>
2024-12-07 17:02:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,8 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
+    import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+    
+
